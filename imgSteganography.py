50dd5a3a63bfa0a226e6e187a626e65eb1eb9259
catjacks38 <jbranchfie@gmail.com>
2021-06-18 07:51:54 -0400

    Fixed a bug, and added some more stuff.
    Fixed bug that would keep appending zips to already existing images.
    Added a successful operation message with the absolute file path of the saved output image.

--- imgSteganography.py
+++ imgSteganography.py
@@ -5,6 +5,10 @@ import os
 def appendDataToImg(imgIn, outImgPath, dataFolderPath):
     shutil.make_archive("temp", "zip", dataFolderPath)
     tempZip = open("temp.zip", "rb")
+
+    if os.path.exists(outImgPath):
+        os.remove(outImgPath)
+
     outImgFile = open(outImgPath, "wb")
 
     outImgFile.write(imgIn.read())
--- main.py
+++ main.py
@@ -19,17 +19,25 @@ def raiseErrorAndExit(error):
     exit(-1)
 
 
-parser = argparse.ArgumentParser(description="A simple program that is used to encode data into images using steganography.")
+def main():
+    parser = argparse.ArgumentParser(description="A simple program that is used to encode data into images using steganography.")
 
-parser.add_argument("--imgIn", type=str, help="The path to the original image you want to encode the data into.", required=True)
-parser.add_argument("--dataFolder", type=str, help="The path to the folder you want to encode into the output image. (Everything inside of the folder will be encoded into the output image--the root folder itself will no be encoded into the output image.)", required=True)
-parser.add_argument("imgOut", type=str, help="The output path of the new image with the data encoded in it. (I would suggest giving the output image the same file extension as the input image, but you do you.)")
+    parser.add_argument("--imgIn", type=str, help="The path to the original image you want to encode the data into.", required=True)
+    parser.add_argument("--dataFolder", type=str, help="The path to the folder you want to encode into the output image. (Everything inside of the folder will be encoded into the output image--the root folder itself will no be encoded into the output image.)", required=True)
+    parser.add_argument("imgOut", type=str, help="The output path of the new image with the data encoded in it. (I would suggest giving the output image the same file extension as the input image, but you do you.)")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-if not os.path.exists(args.imgIn) or not os.path.exists(args.dataFolder):
-    raiseErrorAndExit("File not found. Please check your file paths, and make sure they exist.")
+    if not os.path.exists(args.imgIn) or not os.path.exists(args.dataFolder):
+        raiseErrorAndExit("File not found. Please check your file paths, and make sure they exist.")
 
-imgIn = open(args.imgIn, "rb")
+    imgIn = open(args.imgIn, "rb")
 
-imgSteganography.appendDataToImg(imgIn, args.imgOut, args.dataFolder)
+    imgSteganography.appendDataToImg(imgIn, args.imgOut, args.dataFolder)
+
+    print("Operation Successfully Completed!")
+    print(f"Your file was saved too \"{os.path.abspath(args.imgOut)}\".")
+
+
+if __name__ == "__main__":
+    main()
