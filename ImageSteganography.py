ef1897d4803dd7457f9b57e5cfe0c3d66e0e76db
catjacks38 <jbranchfie@gmail.com>
2021-06-20 13:49:35 -0400

    New feature implementation
    Added "dataToChannel" and "channelToData" methods.
    Implemented those into the CLI.

--- ImageSteganography.py
+++ ImageSteganography.py
@@ -1,5 +1,7 @@
 import shutil
 import os
+import cv2
+import numpy as np
 
 
 def appendDataToImage(imgInData, dataFolderPath, outImgPath):
@@ -20,9 +22,8 @@ def appendDataToImage(imgInData, dataFolderPath, outImgPath):
     return 0
 
 
-def dataToPixels(imgInData, dataFileData, outImgPath):
+def dataToPixels(imgInData, data, outImgPath):
     imgData = bytearray(imgInData)
-    data = bytearray(dataFileData)
 
     if len(data) > len(imgData) - 54:
         return -1
@@ -40,12 +41,58 @@ def dataToPixels(imgInData, dataFileData, outImgPath):
     return 0
 
 
-def pixelsToData(imgInData, outImgPath):
-    if os.path.exists(outImgPath):
-        os.remove(outImgPath)
+def pixelsToData(imgInData, outDataPath):
+    if os.path.exists(outDataPath):
+        os.remove(outDataPath)
 
-    outImg = open(outImgPath, "wb")
+    outImg = open(outDataPath, "wb")
 
     outImg.write(imgInData[54:])
 
     return 0
+
+
+def dataToChannel(inImgPath, data, outImgPath, channel):
+    img = cv2.imread(inImgPath, cv2.IMREAD_UNCHANGED)
+
+    try:
+        flattened = img[:, :, channel].flatten()
+    except:
+        return -2
+
+    dataByteArray = bytearray(data)
+
+    if len(flattened) < len(dataByteArray):
+        return -1
+
+    for i in range(len(dataByteArray)):
+        flattened[i] = dataByteArray[i]
+
+    img[:, :, channel] = np.reshape(flattened, (img.shape[0], img.shape[1]))
+
+    cv2.imwrite(outImgPath, img)
+
+    return 0
+
+
+def channelToData(inImgPath, outDataPath, channel):
+    img = cv2.imread(inImgPath, cv2.IMREAD_UNCHANGED)
+
+    if os.path.exists(outDataPath):
+        os.remove(outDataPath)
+
+    outDataFile = open(outDataPath, "wb")
+
+    try:
+        flattened = img[:, :, channel].flatten()
+    except:
+        return -2
+
+    flattened = flattened.astype(np.byte)
+
+    outData = bytes(flattened)
+
+    outDataFile.write(outData)
+
+    return 0
+
--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import argparse
 from Utils import raiseErrorAndExit
 from sys import exit
 
+
 def main():
     parser = argparse.ArgumentParser(description="A simple program that can be used to encode data into images using steganography.")
 
-    parser.add_argument("--method", "-m", type=str, choices=["append", "dataToPix", "pixToData"], help="The method that will be used to encode/decode the data. The \"append\" method can use any type of image; The \"dataToPix\" and \"pixToData\" methods must be a \".bmp\"/bitmap image type", required=True)
+    parser.add_argument("--method", "-m", type=str, choices=["append", "dataToPix", "pixToData", "dataToChannel", "channelToData"], help="The method that will be used to encode/decode the data.", required=True)
 
     parser.add_argument("--input", "-i", type=str, help="The path to the image you want to decode/encode.", required=True)
-    parser.add_argument("--data", "-d", type=str, help="The path to the data you want to encode into the output image. If the method selected is \"append\", the input path must be a folder. If the method selected is \"pixel\", the input path must be a file.")
+    parser.add_argument("--data", "-d", type=str, help="The path to the data you want to encode into the output image.")
+    parser.add_argument("--channel", "-c", type=int, help="The channel you would like to use to encode/decode the data")
     parser.add_argument("output", type=str, help="The path of where the encoded or decoded data will be saved to.")
 
     args = parser.parse_args()
@@ -18,18 +20,21 @@ def main():
     if not os.path.exists(args.input):
          raiseErrorAndExit("File not found. Please check your file paths, and make sure they exist.")
 
-    if args.method != "pixToData":
+    if args.method != "pixToData" and args.method != "channelToData":
         if bool(args.data):
             if not os.path.exists(args.data):
                 raiseErrorAndExit("File not found. Please check your file paths, and make sure they exist.")
         else:
             raiseErrorAndExit("Missing Data Flag.")
 
-    imgIn = open(args.input, "rb")
-    imgInData = imgIn.read()
+    if (args.method == "dataToChannel" or args.method == "channelToData") and not bool(args.channel):
+        raiseErrorAndExit("Missing channel flag.")
 
     if args.method == "append":
         if os.path.isdir(args.data):
+            imgIn = open(args.input, "rb")
+            imgInData = imgIn.read()
+
             ImageSteganography.appendDataToImage(imgInData, args.data, args.output)
         else:
             raiseErrorAndExit(f"The path, \"{args.data}\" is  not a folder.")
@@ -38,6 +43,9 @@ def main():
         if args.input.split(".")[-1] != "bmp":
             raiseErrorAndExit("Not a \".bmp\"/bitmap type image.")
         if os.path.isfile(args.data):
+            imgIn = open(args.input, "rb")
+            imgInData = imgIn.read()
+
             dataIn = open(args.data, "rb")
             dataInData = dataIn.read()
         else:
@@ -52,7 +60,27 @@ def main():
         if args.input.split(".")[-1] != "bmp":
             raiseErrorAndExit("Not a \".bmp\"/bitmap type image.")
         else:
+            imgIn = open(args.input, "rb")
+            imgInData = imgIn.read()
+
             ImageSteganography.pixelsToData(imgInData, args.output)
+
+    elif args.method == "dataToChannel":
+        dataFile = open(args.data, "rb")
+        data = dataFile.read()
+
+        returnValue = ImageSteganography.dataToChannel(args.input, data, args.output, args.channel - 1)
+
+        if returnValue == -1:
+            raiseErrorAndExit(f"Data file too large for image. Data file size: {len(bytearray(data))}")
+        elif returnValue == -2:
+            raiseErrorAndExit(f"There is no {args.channel} channel in the image.")
+
+    elif args.method == "channelToData":
+        returnValue = ImageSteganography.channelToData(args.input, args.output, args.channel - 1)
+
+        if returnValue == -2:
+            raiseErrorAndExit(f"There is no {args.channel} channel in the image.")
 
     print("Operation Successfully Completed!")
     print(f"Your file was saved too \"{os.path.abspath(args.output)}\".")
