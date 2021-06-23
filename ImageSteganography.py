86b994b222a9c28178e60c7818350a1eca15c6f0
Reep236 <44984940+Reep236@users.noreply.github.com>
2021-06-22 20:06:27 -0400

    Improve dataToChannel and channelToData
    Disperses bytes of data over multiple pixels and translates mean of data to mean of channel.

--- ImageSteganography.py
+++ ImageSteganography.py
@@ -25,7 +25,12 @@ def appendDataToImage(imgInData, dataFolderPath, outImgPath):
 
 
 def dataToChannel(inImgPath, data, outImgPath, channel):
+    # The only material advantage of D2C over by-channel LSB (Implementation In-Progress) is speed
+
     img = cv2.imread(inImgPath, cv2.IMREAD_UNCHANGED)
+
+    # Helps with reading in jpgs, but won't remove existing alpha
+    if img.shape[2] < 4: img = cv2.cvtColor(img, cv2.COLOR_RGB2RGBA)
 
     try:
         flattened = img[:, :, channel].flatten()
@@ -34,11 +39,39 @@ def dataToChannel(inImgPath, data, outImgPath, channel):
 
     dataByteArray = bytearray(data)
 
+
     if len(flattened) < len(dataByteArray):
         return -1
 
-    for i in range(len(dataByteArray)):
-        flattened[i] = dataByteArray[i]
+    # Spread each byte out over multiple pixels
+    pixCount = floor(len(flattened) / len(dataByteArray))
+    splitDBA = [pixCount]
+    for i in dataByteArray:
+        splitDBA.append(floor(i/pixCount) + i%pixCount)
+        for j in range(pixCount-1):
+            splitDBA.append(floor(i/(pixCount)))
+
+    # Makes images look less suspicious by matching data size to image size
+    for i in range(len(flattened)- len(splitDBA) - 2):
+        splitDBA.append(splitDBA[i])
+
+    # Especially helpful for alpha
+    # Localizing would be better, but more costly
+    meanF = round(sum(flattened)/len(flattened))
+    meanD = round(sum(splitDBA)/len(splitDBA))
+    shiftOpt = meanF - meanD
+
+    if max(splitDBA)+shiftOpt > 255: 
+        shift = 255-max(splitDBA)      
+    elif min(splitDBA)+shiftOpt < 0:
+        shift = 0-min(splitDBA)
+    else: 
+        shift = shiftOpt
+
+    flattened[0] = shift
+
+    for i in (range(len(splitDBA))):
+        flattened[i+1] = splitDBA[i] + shift
 
     img[:, :, channel] = np.reshape(flattened, (img.shape[0], img.shape[1]))
 
@@ -60,14 +93,23 @@ def channelToData(inImgPath, outDataPath, channel):
     except:
         return -2
 
-    flattened = flattened.astype(np.byte)
+    shift = -1*(flattened[0])
 
-    outData = bytes(flattened)
+    pixCount = flattened[1] + shift
+    flattenedTmp = []
+
+    for i in range(floor(len(flattened)/pixCount)-2):
+        a = [flattened[(i*pixCount)+2+j]+shift 
+                for j in range(pixCount)]
+        flattenedTmp.append(sum(a))
+
+    
+    flattenedFin = np.array(flattenedTmp).astype(np.byte)
+    outData = bytes(flattenedFin)
 
     outDataFile.write(outData)
 
     return 0
-
 
 def LSBEncode(inImgPath, data, outImgPath, mode):
     inImg = cv2.imread(inImgPath, cv2.IMREAD_UNCHANGED)
